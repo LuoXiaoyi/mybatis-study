5d61618143df456c970a2d6fad3f3275367027f0
Luoxiaoyi <359717521@qq.com>
2018-04-22 14:06:37 +0800

    添加分页插件的实现

---
+++ src/main/java/com/lxy/study/mybatis/domain/models/City2.java
@@ -0,0 +1,118 @@
+/**
+ * [Product] mybatis.study [Copyright] Copyright © 2018 ZTESoft All Rights Reserved. [FileName]
+ * City.java [History] Version  Date      Author     Content -------- --------- ----------
+ * ------------------------ 1.0.0    Apr 15, 2018   Luo Xiaoyi    最初版本
+ */
+package com.lxy.study.mybatis.domain.models;
+
+/**
+ * <p>
+ * Class: City
+ * </p>
+ * Description:
+ *
+ * @author Luo.xiaoyi
+ *
+ */
+public class City2 {
+
+  private int id;
+  private String name;
+  private Country country;
+  private String district;
+  private int population;
+  private Role role;
+
+  /**
+   * @return the role
+   */
+  public Role getRole() {
+    return role;
+  }
+
+  /**
+   * @param role
+   *            the role to set
+   */
+  public void setRole(Role role) {
+    this.role = role;
+  }
+
+  /**
+   * @return the id
+   */
+  public int getId() {
+    return id;
+  }
+
+  /**
+   * @param id
+   *            the id to set
+   */
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  /**
+   * @return the name
+   */
+  public String getName() {
+    return name;
+  }
+
+  /**
+   * @param name
+   *            the name to set
+   */
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Country getCountry() {
+    return country;
+  }
+
+  public void setCountry(Country country) {
+    this.country = country;
+  }
+
+  /**
+   * @return the district
+   */
+  public String getDistrict() {
+    return district;
+  }
+
+  /**
+   * @param district
+   *            the district to set
+   */
+  public void setDistrict(String district) {
+    this.district = district;
+  }
+
+  /**
+   * @return the population
+   */
+  public int getPopulation() {
+    return population;
+  }
+
+  /**
+   * @param population
+   *            the population to set
+   */
+  public void setPopulation(int population) {
+    this.population = population;
+  }
+
+  /* (non-Javadoc)
+   * @see java.lang.Object#toString()
+   */
+  @Override
+  public String toString() {
+    return "City [id=" + id + ", name=" + name + ", country: {" + country + "}, district="
+        + district + ", population=" + population + ", role=" + role + "]";
+  }
+
+}
